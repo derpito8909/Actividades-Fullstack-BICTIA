e6b8456bc133c255ee80d24e495c53d34c1350e0
David Rodriguez <davisito544@gmail.com>
2020-09-17 11:51:45 -0500

    ejercicio de local storage

---
+++ javascript/assets/src/js/script2.js
@@ -0,0 +1,5 @@
+let nombre = document.getElementById('name');
+let apellido = document.getElementById('surname');
+let email = document.getElementById('mail');
+
+let userData = JSON.parse(localStorage.getItem(datos));
