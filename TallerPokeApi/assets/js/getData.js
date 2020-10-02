eb562d214d2f2157beb9e2b0c4b2f5fdd596d9bc
David Rodriguez <davisito544@gmail.com>
2020-10-02 16:36:47 -0500

    Version 3

--- TallerPokeApi/assets/js/getData.js
+++ TallerPokeApi/assets/js/getData.js
@@ -8,15 +8,15 @@ const getDataAPI = (api) => {
     return fetch(api)
         .then((response) => response.json())
         .then((json) => {
-            getAllPokemons(json.results);
+            getAllPokemons(json), paginacion(json)
         })
         .catch((error) => {
             console.log("Error", error);
         })
 }
 const getAllPokemons = (data) => {
-    for (let i = 1; i < data.length; i++) {
-        getPokemon(i);        
+    for (let i = 1; i < data.results.length; i++) {
+        getPokemon(i);
     }
 }
 //Obtener cada id de los poqkemones
@@ -33,32 +33,34 @@ const getPokemon = (id) => {
         })
 }
 const mostarPokemon = (data) => {
-    console.log(data);
-    let createElementHtml = "";
-    createElementHtml += '<div class="col">';
-    createElementHtml += '<div class="card" style="width:10rem;">';
-    createElementHtml += `<img src="${data.sprites.front_default}" class="card-img-top" alt="Card image cap">`;
-    createElementHtml += '<div class="card-body">';
-    createElementHtml += `<h5 class="card-title">${data.name}</h5>`;
-    createElementHtml += `<p class="card-text">Abilities: ${data.abilities[0].ability.name}</p>`;
-    createElementHtml += `<p class="card-text">Type: ${data.types[0].type.name}</p>`;
-    createElementHtml += `<p class="card-text">Height: ${data.height}</p>`;
-    createElementHtml += `<p class="card-text">Weight: ${data.weight}</p>`;
-    createElementHtml += '</div>';
-    createElementHtml += '</div>';
-    createElementHtml += '</div>';
-    document.getElementById('datosPersonajes').innerHTML = createElementHtml;
+    const listPokemonDiv = document.createElement('div');
+    listPokemonDiv.classList.add('col');
+
+    let pokemonCardBody = "";
+    pokemonCardBody += '<div class="col">';
+    pokemonCardBody += '<div class="card" style="width:10rem;">';
+    pokemonCardBody += `<img src="${data.sprites.front_default}" class="card-img-top" alt="Card image cap">`;
+    pokemonCardBody += '<div class="card-body">';
+    pokemonCardBody += `<h5 class="card-title">${data.name}</h5>`;
+    pokemonCardBody += `<p class="card-text">Abilities: ${data.abilities[0].ability.name}</p>`;
+    pokemonCardBody += `<p class="card-text">Type: ${data.types[0].type.name}</p>`;
+    pokemonCardBody += `<p class="card-text">Height: ${data.height}</p>`;
+    pokemonCardBody += `<p class="card-text">Weight: ${data.weight}</p>`;
+    pokemonCardBody += '</div>';
+    pokemonCardBody += '</div>';
+    pokemonCardBody += '</div>';
+    listPokemonDiv.innerHTML = pokemonCardBody;
+    document.getElementById('datosPersonajes').appendChild(listPokemonDiv);
 }
 // Paginacion
 const paginacion = (info) => {
+    console.log(info);
     let prevDisabled = "";
     let nextDisabled = "";
     let createElementHtml = "";
     createElementHtml += `<li class="page-item ${info.prev == null ? prevDisabled = "disabled": prevDisabled = ""} "><a class="page-link" onlick="getData('${info.prev}')">Previous</a></li>`;
     createElementHtml += `<li class="page-item ${info.next == null ? nextDisabled = "disabled": nextDisabled = ""}"><a class="page-link" onlick="getData('${info.next}')">Next</a></li>`;
-    console.log(createElementHtml);
     document.getElementById("paginacion").innerHTML = createElementHtml;
-    console.log(API);
 }
 //EjecutarData
 getDataAPI(API);
